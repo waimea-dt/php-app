2cc555de4670be736a0f14c0692a5b7bfed5e975
Steve Copley <cpy@dt.waimea.school.nz>
2024-06-24 12:20:45 +1200

    Added a dist version

---
+++ dist/index.php
@@ -0,0 +1,16 @@
+
+<?php require_once '_config.php'; ?>
+
+<?php require 'partials/top.php'; ?>
+
+    <?php require 'partials/header.php'; ?>
+
+    <main>
+
+        <h1>Hello from the PHP Web App!</h1>
+    
+    </main>
+
+    <?php require 'partials/footer.php'; ?>
+
+<?php require 'partials/bottom.php'; ?>
